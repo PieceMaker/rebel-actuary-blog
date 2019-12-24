4351953ee0b7ced4fdedffeaedf871016c1bd61f
Jonathan Adams <jd.adams16@gmail.com>
2019-12-23 19:42:56 -0600

    Add GitHub and Twitter links to navbar.

--- theme/components/navbar.vue
+++ theme/components/navbar.vue
@@ -4,6 +4,14 @@
             <b-nav-item href="/">Home</b-nav-item>
             <b-nav-item href="/about.html">About Me</b-nav-item>
         </b-navbar-nav>
+        <b-navbar-nav class="ml-auto">
+            <b-nav-item href="https://github.com/PieceMaker">
+                <fa :icon="[ 'fab', 'github' ]"></fa>
+            </b-nav-item>
+            <b-nav-item href="https://twitter.com/RebelActuary">
+                <fa :icon="[ 'fab', 'twitter' ]"></fa>
+            </b-nav-item>
+        </b-navbar-nav>
     </b-navbar>
 </template>
 
